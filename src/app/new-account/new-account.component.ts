5cd2c9aaa5597fe0fbfad454fe364c171ab8e51f
daisymae <daisymae@orcadiafarm.com>
2018-04-28 16:06:39 -0500

    Use Logging Service
    Correctly use logging service in components.

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -4,19 +4,21 @@ import { LoggingService } from '../logging.service';
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+  styleUrls: ['./new-account.component.css'],
+  providers: [LoggingService]
 })
 export class NewAccountComponent {
   @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+
+  // inject the service in the constructor
+  constructor(private logService: LoggingService) {}
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountAdded.emit({
       name: accountName,
       status: accountStatus
     });
-    // THIS IS THE WRONG WAY TO USE A SERVICE IN ANGULAR!
-    const service = new LoggingService();
-    service.logStatusChange(accountStatus);
+    this.logService.logStatusChange(accountStatus);
     // console.log('A server status changed, new status: ' + accountStatus);
   }
 }
