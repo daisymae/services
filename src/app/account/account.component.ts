626eef2c0f859207e75fc30f934df5f8f72ff425
daisymae <daisymae@orcadiafarm.com>
2018-04-28 16:45:27 -0500

    Communicate between components
    Using Services to communicate between components.

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -5,20 +5,18 @@ import { AccountsService } from '../accounts.service';
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
-  styleUrls: ['./account.component.css'],
-  // providers: [LoggingService  ]
+  styleUrls: ['./account.component.css']
 })
 export class AccountComponent {
   @Input() account: { name: string, status: string };
   @Input() id: number;
 
   // inject the service in the constructor
-  constructor(private logService: LoggingService,
-    private accountsService: AccountsService) { }
+  constructor(private accountsService: AccountsService) { }
 
   onSetTo(status: string) {
     this.accountsService.updateStatus(this.id, status);
-    // this.logService.logStatusChange(status);
-    // console.log('A server status changed, new status: ' + status);
+    // emitting event from accountsService
+    this.accountsService.statusUpdated.emit(status);
   }
 }
--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -5,23 +5,19 @@ import { AccountsService } from '../accounts.service';
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css'],
-  // providers: [LoggingService]
+  styleUrls: ['./new-account.component.css']
 })
 export class NewAccountComponent {
-  // @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
 
   // inject the service in the constructor
   constructor(private logService: LoggingService,
-  private accountsService: AccountsService) {}
+  private accountsService: AccountsService) {
+    this.accountsService.statusUpdated.subscribe(
+      (status: string) => alert('New Status: ' + status)
+    );
+  }
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    // this.accountAdded.emit({
-    //   name: accountName,
-    //   status: accountStatus
-    // });
     this.accountsService.addAccount(accountName, accountStatus);
-    // this.logService.logStatusChange(accountStatus);
-    // console.log('A server status changed, new status: ' + accountStatus);
   }
 }
